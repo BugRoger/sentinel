f820ca8c6f52aa1f5ffd441d6f0aeeb66b10ba49
Michael Schmidt <michael.j.schmidt@gmail.com>
2011-09-17 19:19:35 +0200

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sentinel::Application.config.secret_token = '99c3ea222b7b8d856efc81d0539decff6e14544b0139484b59af33f3fabfd31285c950cfb68a2bf0b5e8c1e2c06cf086f761fb59f3ca806bf6514f8981c8e948'
